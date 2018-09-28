55bd59c00c1000945546207b6248df4c37ce1e0f
hanjt <hjt950722@163.com>
2018-09-28 17:34:10 +0800

    Java位运算符

--- messStudy/src/com/jackPotHan/myTest/Java10/JDK10Test.java
+++ messStudy/src/com/jackPotHan/myTest/Java10/JDK10Test.java
@@ -3,6 +3,7 @@ package com.jackPotHan.myTest.Java10;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @Author: hanjt
@@ -13,11 +14,15 @@ public class JDK10Test {
     public static void main(String[] args) throws MalformedURLException {
         var map = new HashMap();
 
+        var list = List.of("girlFriend", "boyFriend");
+
         var git = new URL("https://github.com/JackpotHan");
 
         var thought = "Java more and more like as Groovy";
 
         var notice = "You need Java 10 Environment";
+
+        var talk = "What do you want to do during The Country Day ?  I don't know,but I know you will know that, are you ?";
 
         map.put("name", "JackpotHan");
         map.put("age", 23);
@@ -25,7 +30,8 @@ public class JDK10Test {
         map.put("github", git);
         map.put("thought", thought);
         map.put("notice", notice);
-
+        map.put("talk", talk);
+        map.put("girls", list);
         System.out.println("About Me:" + map);
     }
 }
