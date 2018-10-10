296cd4c907cbda81fa1d99bf779c63debbbeebff
hanjt <hjt950722@163.com>
2018-10-10 13:41:51 +0800

    Java IO

---
+++ ioStudy/src/com/jackpotHan/copy/CopyFileByByte.java
@@ -0,0 +1,43 @@
+package com.jackpotHan.copy;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @Author: hanjt
+ * @Date: 2018/10/10 12:07
+ * @Description:
+ */
+public class CopyFileByByte {
+    public static void main(String[] args) {
+
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            fis = new FileInputStream("./\\ioStudy\\src\\com\\jackpotHan\\music.txt");
+            fos = new FileOutputStream("./\\ioStudy\\src\\com\\jackpotHan\\music_copy.txt");
+            // 每次读取1024 bytes
+            byte input[] = new byte[1024];
+            // read返回读入的数据大小，如果没有数据返回-1
+            while (fis.read(input) != -1) {
+                // 每次写入1024 bytes
+                fos.write(input);
+            }
+
+            System.out.println("Done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
